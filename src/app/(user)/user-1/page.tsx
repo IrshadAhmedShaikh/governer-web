efa6662d0731ff80b008ddbc68bbd6ec207d74cc
Irshad Ahmed <airshadahmedshaikh@gmail.com>
2025-01-19 15:56:25 +0500

    first commit

---
+++ src/app/(user)/user-1/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>user-1</div>
+  )
+}
+
+export default page
---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='text-center content-center text-3xl font-bold'>This is Footer</div>
+  )
+}
+
+export default Footer
